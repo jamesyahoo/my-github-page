48be554b54e9a1aa3440ec9a2cdcfcad389f90f3
jamesyahoo <jayantamanna01@gmail.com>
2024-06-26 00:28:55 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const alertButton = document.getElementById("alertButton");
+    alertButton.addEventListener("click", function() {
+        alert("Hello! You clicked the button.");
+    });
+});
